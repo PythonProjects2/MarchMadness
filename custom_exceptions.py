a7bf112b1c016cd997adede031991b6dec3606b2
Raragyay <let987let987@gmail.com>
2018-03-06 20:19:42 -0500

    Added Description

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -1,3 +1,5 @@
 class DisallowedConnection(Exception):
+    # We create a custom exception so that we can get the status code as well.
+    # This way, it can be printed when we catch that exception.
     def __init__(self, status):
         self.status = status
